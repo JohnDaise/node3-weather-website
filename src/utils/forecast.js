9de1945d8fdae537f40f5ce823df891abbb34035
JohnDaise <jdaise@gmail.com>
2019-12-10 11:53:05 -0500

    added hi and lo temperature to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,8 +7,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to connect to weather service', undefined);
        } else if (body.error) {
             callback('Unable to find location', undefined);
-       } else {     
-        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.');
+       } else {
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. High: ' + body.daily.data[0].temperatureHigh + '° Low: ' + body.daily.data[0].temperatureLow + '°');
         }
      });
 
